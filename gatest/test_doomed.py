d883ddcd1ad2901592e8edd7c825d1f25cf95704
Sean Charles <sean.charles@mitigatecyber.com>
2021-09-09 15:26:44 +0100

    borked again, review PR

--- gatest/test_doomed.py
+++ gatest/test_doomed.py
@@ -2,6 +2,8 @@ from django.test import SimpleTestCase
 
 class DeathOrGloryTest(SimpleTestCase):
     def test_always_fails(self):
-        # failit: Test will fail
-        self.assertEqual(0,0)
+        # f3review: not only will this fail but it will need
+        # manual reviewer intervention as the branch rule now
+        # insists on minumum 1 reviewer.
+        self.assertEqual(42,100)
 

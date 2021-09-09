f7eec1e7e09cd2f6b71acf253894303d4923f81f
Sean Charles <sean.charles@mitigatecyber.com>
2021-09-09 14:03:24 +0100

    failed

--- gatest/test_doomed.py
+++ gatest/test_doomed.py
@@ -2,7 +2,6 @@ from django.test import SimpleTestCase
 
 class DeathOrGloryTest(SimpleTestCase):
     def test_always_fails(self):
-        # main: Test will pass
         # failit: Test will fail
-        self.assertTrue(True)
+        self.assertTrue(False)
 
